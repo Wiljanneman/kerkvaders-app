3048f064caa6614a3555114a8928080c380cd937
Wiljanneman <williamniemand94@gmail.com>
2025-04-10 22:30:36 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import maplibregl from 'maplibre-gl';
 import { NgxMatTimelineModule } from 'ngx-mat-timeline';
+import '@maplibre/maplibre-gl-leaflet';
+import {filterByDate, dateRangeFromISODate } from '@openhistoricalmap/maplibre-gl-dates';
+
 
 interface Record {
   icon?: string | null;
@@ -17,37 +21,61 @@ interface Record {
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    // var map = (L as any).map("map").setView([38.912753, -77.032194], 15);
+
+    // const map = new maplibregl.Map({
+    //   container: 'map',
+    //   style: 'https://www.openhistoricalmap.org/map-styles/main/main.json',
+    //   attributionControl: {
+    //     customAttribution: '<a href="https://www.openhistoricalmap.org/">OpenHistoricalMap</a>',
+    //   },
+    // });
+
+    // // const maplibreMap = map.getMaplibreMap();
+    // // const maplibreMap = (map as any).getMaplibreMap();
+    // map.once('styledata', function (e: any) {
+    //    filterByDate(map, '1890-04-14');
+    // });
+
+
+  }
   title = 'kerkvaders-app';
 
   items: Record[] = [
     {
       icon: 'hiking',
-      label: '1998',
+      label: 'Clement of Alexandria',
+      iconUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSivTpOkL0X6cz_vplnK1Pzup9i09cPPxok3w&s",
       content:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
     {
       icon: 'directions_run',
-      label: '2003',
+      iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Origen.jpg/250px-Origen.jpg',
+      label: 'Origen',
       content:
         "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. ",
     },
     {
-      iconUrl: 'avatar.jpg',
-      label: '2007',
+      iconUrl: 'https://breakpoint.org/wp-content/uploads/2023/12/Copy-of-BP-Picture-219.png',
+      label: 'Athanasius ',
       content:
         "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
     },
     {
       icon: 'directions_bike',
-      label: '2012',
+      label: 'Augustine',
+      iconUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQarHK20HLc3Y5sLvDw4mU6ah0INM4NXQqXoQ&s',
       content:
         'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
     },
     {
       icon: 'language',
-      label: '2020',
+      iconUrl: 'https://fatima.org/wp-content/uploads/2021/01/Fatima-Messages_Saints-St.-John-Chrysostom-01-800x518.jpg',
+      label: 'John Chrysostom',
       content:
         'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
     },
